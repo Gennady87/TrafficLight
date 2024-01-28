b35a6aa2ef53cb6eb009f3dd397883fe17751c32
Gennady87 <77787779+Gennady87@users.noreply.github.com>
2024-01-29 03:46:56 +0700

    functional realization

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var lightButton: UIButton!
+    
+    let lightIsOff: CGFloat = 0.3
+    let lightIsOn: CGFloat = 1
+    
+    var currentLight = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        redLight.layer.cornerRadius = 70
+        yellowLight.layer.cornerRadius = 70
+        greenLight.layer.cornerRadius = 70
+        
+        lightButton.layer.cornerRadius = 10
+        
     }
 
 
+    @IBAction func lightButtonDidTapped() {
+        lightButton.setTitle(redLight.alpha == lightIsOff && yellowLight.alpha == lightIsOff && greenLight.alpha == lightIsOff ? "Start" : "Next", for: .normal)
+        
+        if currentLight == "" {
+            redLight.alpha = lightIsOn
+            currentLight = "Red"
+        } else if currentLight == "Red" {
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = "Yellow"
+        } else if currentLight == "Yellow" {
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = "Green"
+        } else if currentLight == "Green" {
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = "Red"
+        }
+    }
 }
 
